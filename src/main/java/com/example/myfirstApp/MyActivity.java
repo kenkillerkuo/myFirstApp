580b30dce780f4a01befca7a538ab91ce402ab4f
keniakuo <keniakuo@synology.com>
2016-04-28 16:51:13 +0800

    [app bar] - add toolbar buttons: search and setting
    ref: http://hukai.me/android-training-course-in-chinese/basics/actionbar/styling.html

--- src/main/java/com/example/myfirstApp/MyActivity.java
+++ src/main/java/com/example/myfirstApp/MyActivity.java
@@ -3,6 +3,8 @@ package com.example.myfirstApp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.content.Intent;
 import android.widget.EditText;
 
@@ -23,6 +25,13 @@ public class MyActivity extends AppCompatActivity
 		setSupportActionBar(myToolbar);
     }
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+			super.onCreateOptionsMenu(menu);
+			getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+			return true;
+	}
+
 	/** Called when the user clicks the Send button */
 	public void sendMessage(View view) {
 			Intent intent = new Intent(this, DisplayMessageActivity.class);
@@ -31,5 +40,25 @@ public class MyActivity extends AppCompatActivity
 
 			intent.putExtra(EXTRA_MESSAGE, message);
 			startActivity(intent);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+			switch (item.getItemId()) {
+					case R.id.action_settings:
+							// User chose the "Settings" item, show the app settings UI...
+							return true;
+
+					case R.id.action_search:
+							// User chose the "Favorite" action, mark the current item
+							// as a favorite...
+							return true;
+
+					default:
+							// If we got here, the user's action was not recognized.
+							// Invoke the superclass to handle it.
+							return super.onOptionsItemSelected(item);
+
+			}
 	}
 }
