f664cda5ca9a77a8eaf9d8d2f290f75573d7f0e2
keniakuo <keniakuo@synology.com>
2016-04-28 17:07:30 +0800

    [app bar] - enable the up button on the toolbar
    ref: http://developer.android.com/intl/zh-tw/training/appbar/up-action.html

--- src/main/java/com/example/myfirstApp/DisplayMessageActivity.java
+++ src/main/java/com/example/myfirstApp/DisplayMessageActivity.java
@@ -6,22 +6,30 @@ import android.view.View;
 import android.content.Intent;
 import android.widget.TextView;
 
-public class DisplayMessageActivity extends Activity
+import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
+
+public class DisplayMessageActivity extends AppCompatActivity
 {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.display_message);
 
-		Intent intent = getIntent();
-		String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+			Intent intent = getIntent();
+			String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
 
-		TextView textView = new TextView(this);
-		textView.setTextSize(40);
-		textView.setText(message);
+			TextView textView = (TextView) findViewById(R.id.message);
+			textView.setTextSize(40);
+			textView.setText(message);
 
-		setContentView(textView);
+			Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+			setSupportActionBar(myToolbar);
+
+			//Enable the Up Button
+			getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
